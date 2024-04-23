a7822ee81b1e8432905c999049a666bfaa290dfa
Jean Pondy <manbayesse@gmail.com>
2024-04-23 05:21:24 +0200

    models/movable-object.class.js

--- js/game.js
+++ js/game.js
@@ -1,12 +1,9 @@
 let canvas;
 let ctx; // mit context kann man funltion aufrufen
-let character = new Image();
+let character = new MovableObject();
 
 function init() {
   canvas = document.getElementById("canvas");
   ctx = canvas.getContext("2d");
-  character.src = "../img/2_character_pepe/2_walk/W-21.png";
-  setTimeout(function () {
-    ctx.drawImage(character, 20, 20, 50, 150);
-  }, 2000);
+  console.log("My Character is", character);
 }

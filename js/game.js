ec84c82c585c620ed12173c9b47bab95a7aca7d2
Jean Pondy <manbayesse@gmail.com>
2024-04-23 10:40:01 +0200

    Character anzeigen

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
 let canvas;
-let ctx; // mit context kann man funltion aufrufen
 
-let world = new World();
+let world;
 
 function init() {
   canvas = document.getElementById("canvas");
-  ctx = canvas.getContext("2d");
+  world = new World(canvas);
+
   console.log("My Character is", world.character);
   console.log("My enemies are", world.enemies);
 }
--- models/character.class.js
+++ models/character.class.js
@@ -1,6 +1,7 @@
 class Character extends MovableObject {
   constructor() {
     super();
+    // 2.Bilder Einfügen
     this.loadImage("./img/2_character_pepe/2_walk/W-21.png");
   }
   jump() {}

f4cee8bb675090a2e34727b07085d3f30c196910
Jean Pondy <manbayesse@gmail.com>
2024-04-24 22:00:16 +0200

    Hühnchen platzieren/ Math.random()

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,5 +3,6 @@ class Chicken extends MovableObject {
     super();
     // loadImage-Methode aufrufen, um das Bild zu laden
     this.loadImage("./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
+    this.x = 200 + Math.random() * 500;
   }
 }

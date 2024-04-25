0ad0c35062ce1e59ebea527e900740336393c2e5
Jean Pondy <manbayesse@gmail.com>
2024-04-25 13:41:22 +0200

    Character animieren

--- models/character.class.js
+++ models/character.class.js
@@ -3,18 +3,31 @@ class Character extends MovableObject {
   height = 290; // Standardhöhe festlegen
   x = 120;
   y = 143;
+  IMAGES_WALKING = [
+    "./img/2_character_pepe/2_walk/W-21.png",
+    "./img/2_character_pepe/2_walk/W-22.png",
+    "./img/2_character_pepe/2_walk/W-23.png",
+    "./img/2_character_pepe/2_walk/W-24.png",
+    "./img/2_character_pepe/2_walk/W-25.png",
+    "./img/2_character_pepe/2_walk/W-26.png",
+  ];
+  currentImage = 0;
   constructor() {
     super();
     // 2.Bilder Einfügen
     this.loadImage("./img/2_character_pepe/2_walk/W-21.png");
-    this.loadImages([
-      "./img/2_character_pepe/2_walk/W-21.png",
-      "./img/2_character_pepe/2_walk/W-22.png",
-      "./img/2_character_pepe/2_walk/W-23.png",
-      "./img/2_character_pepe/2_walk/W-24.png",
-      "./img/2_character_pepe/2_walk/W-25.png",
-      "./img/2_character_pepe/2_walk/W-26.png",
-    ]);
+    this.loadImages(this.IMAGES_WALKING);
+    this.animate();
   }
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 7%6; Rest 1
+      // i = 0,1,2,3,4,5, 0,1,2,3,4,5, 0,1,2,3,4,5,0 ....
+      let path = this.IMAGES_WALKING[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 1000);
+  }
+
   jump() {}
 }

baf01db293004495d4a388d676ccbb05364878b0
Jean Pondy <manbayesse@gmail.com>
2024-04-25 15:04:20 +0200

    Chicken bewegen

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -16,10 +16,12 @@ class Chicken extends MovableObject {
     this.loadImage("./img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
     this.loadImages(this.IMAGES_WALKING);
     this.x = 200 + Math.random() * 500;
+    this.speed = 0.15 + Math.random() * 0.5; //Chicken mit unterschiedlich geschwindigkeit laufen
     this.animate();
   }
 
   animate() {
+    this.moveLeft();
     setInterval(() => {
       let i = this.currentImage % this.IMAGES_WALKING.length; //let i = 7%6; Rest 1
       // i = 0,1,2,3,4,5, 0,1,2,3,4,5, 0,1,2,3,4,5,0 ....
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -7,13 +7,11 @@ class Cloud extends MovableObject {
 
     // loadImage-Methode aufrufen, um das Bild zu laden
     this.loadImage("./img/5_background/layers/4_clouds/1.png");
-    this.x = 0 + Math.random() * 500 - 1;
+    this.x = 0 + Math.random() * 500;
     this.animate();
   }
 
   animate() {
-    setInterval(() => {
-      this.x -= 0.15;
-    }, 1000 / 60);
+    this.moveLeft();
   }
 }

32ff7e3e470979a5ee174d14c826f52849c5f0c0
Jean Pondy <manbayesse@gmail.com>
2024-04-25 15:15:58 +0200

    Keyboard Objekt

--- js/game.js
+++ js/game.js
@@ -9,3 +9,8 @@ function init() {
   console.log("My Character is", world.character);
   console.log("My enemies are", world.enemies);
 }
+
+// ob ein Taske nicht gedrückt ist oder losgelassen ist // Keyboard Objekt
+window.addEventListener("keypress", (e) => {
+  console.log(e);
+});

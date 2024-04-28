bceaea5887a1469b6962467017bd9b41058d3262
Jean Pondy <manbayesse@gmail.com>
2024-04-28 15:42:14 +0200

    Collision detection + update cloud

--- models/world.class.js
+++ models/world.class.js
@@ -47,6 +47,13 @@ class World {
       mo.x = mo.x * -1;
     }
     this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+
+    this.ctx.lineWidth = "2";
+    this.ctx.strokeStyle = "blue";
+    this.ctx.beginPath();
+    this.ctx.rect(mo.x, mo.y, mo.x + mo.width, mo.y + mo.height);
+    this.ctx.stroke();
+
     if (mo.otherDirection) {
       mo.x = mo.x * -1;
       this.ctx.restore();

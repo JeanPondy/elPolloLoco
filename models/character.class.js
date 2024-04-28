8d77185affbefd9c5b6fddeeac128dc6b18571ba
Jean Pondy <manbayesse@gmail.com>
2024-04-28 12:51:55 +0200

    Jump animationen anzeigen

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,18 @@ class Character extends MovableObject {
     "./img/2_character_pepe/2_walk/W-25.png",
     "./img/2_character_pepe/2_walk/W-26.png",
   ];
+
+  IMAGES_JUMPING = [
+    "./img/2_character_pepe/3_jump/J-31.png",
+    "./img/2_character_pepe/3_jump/J-32.png",
+    "./img/2_character_pepe/3_jump/J-33.png",
+    "./img/2_character_pepe/3_jump/J-34.png",
+    "./img/2_character_pepe/3_jump/J-35.png",
+    "./img/2_character_pepe/3_jump/J-36.png",
+    "./img/2_character_pepe/3_jump/J-37.png",
+    "./img/2_character_pepe/3_jump/J-38.png",
+    "./img/2_character_pepe/3_jump/J-39.png",
+  ];
   world;
   walking_sound = new Audio("audio/running2.mp3");
 
@@ -20,6 +32,7 @@ class Character extends MovableObject {
     // 2.Bilder Einfügen
     this.loadImage("./img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_JUMPING);
     this.applyGravity();
     this.animate();
   }
@@ -42,9 +55,13 @@ class Character extends MovableObject {
     }, 1000 / 60);
 
     setInterval(() => {
-      if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-        // walk animation
-        this.playAnimation(this.IMAGES_WALKING);
+      if (this.isAboveGround()) {
+        this.playAnimation(this.IMAGES_JUMPING);
+      } else {
+        if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+          // walk animation
+          this.playAnimation(this.IMAGES_WALKING);
+        }
       }
     }, 1000 / 20);
   }

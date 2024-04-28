15cadfd4581cbe72fa76bbc74fe1f31367a02098
Jean Pondy <manbayesse@gmail.com>
2024-04-28 16:10:57 +0200

    Refactoring

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -29,6 +29,19 @@ class MovableObject {
     this.img = new Image();
     this.img.src = path;
   }
+
+  draw(ctx) {
+    ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+  }
+
+  drawFrame(ctx) {
+    ctx.lineWidth = "2";
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.stroke();
+  }
+
   /**
    *
    * @param {Array} arr - ['img/image1.png','img/image1.png', ... ]
--- models/world.class.js
+++ models/world.class.js
@@ -41,22 +41,25 @@ class World {
   // extra Funktion
   addToMap(mo) {
     if (mo.otherDirection) {
-      this.ctx.save(); // alle Eigenschaft von ctx speichern
-      this.ctx.translate(mo.width, 0); // Wir spiegeln das Bild um 180° andere Richtung
-      this.ctx.scale(-1, 1); // nach recht verschieben
-      mo.x = mo.x * -1;
+      this.flipImage(mo);
     }
-    this.ctx.drawImage(mo.img, mo.x, mo.y, mo.width, mo.height);
+    mo.draw(this.ctx);
 
-    this.ctx.lineWidth = "2";
-    this.ctx.strokeStyle = "blue";
-    this.ctx.beginPath();
-    this.ctx.rect(mo.x, mo.y, mo.x + mo.width, mo.y + mo.height);
-    this.ctx.stroke();
+    mo.drawFrame(this.ctx);
 
     if (mo.otherDirection) {
-      mo.x = mo.x * -1;
-      this.ctx.restore();
+      this.flipImageBack(mo);
     }
+  }
+
+  flipImage(mo) {
+    this.ctx.save(); // alle Eigenschaft von ctx speichern
+    this.ctx.translate(mo.width, 0); // Wir spiegeln das Bild um 180° andere Richtung
+    this.ctx.scale(-1, 1); // nach recht verschieben
+    mo.x = mo.x * -1;
+  }
+  flipImageBack(mo) {
+    mo.x = mo.x * -1;
+    this.ctx.restore();
   }
 }

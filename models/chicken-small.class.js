f6dbb7e6e036cf7acb8f7b68557e0754b48527cd
Jean Pondy <manbayesse@gmail.com>
2024-05-16 03:32:56 +0200

    ChickenSmall updated

--- models/chicken-small.class.js
+++ models/chicken-small.class.js
@@ -15,6 +15,8 @@ class ChickenSmall extends MovableObject {
     "./img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
     "./img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
   ];
+
+  IMAGES_DEAD = ["img/3_enemies_chicken/chicken_small/2_dead/dead.png"];
   chicken_sound = new Audio("audio/chicken.mp3"); // Audio-Objekt für Laufgeräusche
 
   constructor() {
@@ -24,6 +26,7 @@ class ChickenSmall extends MovableObject {
     this.loadImage("./img/3_enemies_chicken/chicken_small/1_walk/1_w.png");
     // Laden der Bilder für die Laufanimation des Hühnchens
     this.loadImages(this.IMAGES_WALKING);
+    this.loadImages(this.IMAGES_DEAD);
     this.y = 390;
     // Zufällige X-Position für das Hühnchen innerhalb eines bestimmten Bereichs
     this.x = 400 + Math.random() * 1700;
@@ -43,14 +46,14 @@ class ChickenSmall extends MovableObject {
       }
       // Abspielen des Laufgeräusches, wenn das Huhn zu laufen beginnt
       //this.chicken_sound.play();
-    }, 1000 / 60); // Aktualisierungsgeschwindigkeit der Bewegung (60 Frames pro Sekunde)
+    }, 1000 / 20); // Aktualisierungsgeschwindigkeit der Bewegung (60 Frames pro Sekunde)
     this.chickenSmallMoveLeft(); // Bewegung des Hühnchens nach links
     this.chickenSmallWalk();
   }
 
   chickenSmallAttack() {
     if (this.attack) {
-      this.speed = 5.5;
+      this.speed = 2.5;
     }
   }
   chickenSmallDead() {
@@ -75,64 +78,3 @@ class ChickenSmall extends MovableObject {
     }, 1000 / 60);
   }
 }
-/*   constructor() {
-    super(); // Aufruf des Konstruktors der Elternklasse (MovableObject)
-
-    this.x = 400 + Math.random() * 1700;
-    this.y = 390;
-    this.height = 40;
-    this.width = 40;
-    this.offset = {
-      top: 8,
-      left: 8,
-      right: 8,
-      bottom: 8,
-    };
-    this.speed = 0.15 + Math.random() * 0.5;
-
-    this.IMAGES_WALKING = [
-      "./img/3_enemies_chicken/chicken_small/1_walk/1_w.png",
-      "./img/3_enemies_chicken/chicken_small/1_walk/2_w.png",
-      "./img/3_enemies_chicken/chicken_small/1_walk/3_w.png",
-    ];
-    this.IMAGES_DEAD = ["./img/3_enemies_chicken/chicken_small/dead.png"];
-
-    // Laden der Bilder für die Laufanimation des Hühnchens
-    this.loadImages(this.IMAGES_WALKING);
-
-    // Animationen starten
-    this.startAnimations();
-  }
-
-  startAnimations() {
-    // Periodische Bewegung des Hühnchens nach links
-    this.chickenSmallAnimations = setInterval(() => {
-      if (this.isDead()) {
-        this.chickenSmallDead();
-      } else {
-        this.chickenSmallMoveLeft(); // Bewegung des Hühnchens nach links
-        this.chickenSmallWalk(); // Laufanimation des Hühnchens abspielen
-      }
-    }, 1000 / 60); // Aktualisierungsgeschwindigkeit der Bewegung (60 Frames pro Sekunde)
-  }
-
-  // Periodisches Abspielen der Laufanimation des Hühnchens
-  chickenSmallWalk() {
-    this.playAnimation(this.IMAGES_WALKING); // Abspielen der Laufanimation
-  }
-
-  // Bewegung des Hühnchens nach links
-  chickenSmallMoveLeft() {
-    this.moveLeft(); // Bewegung des Hühnchens nach links
-  }
-
-  // Methode zur Behandlung des Todes des Hühnchens
-  chickenSmallDead() {
-    this.active = false; // Huhn als inaktiv markieren
-    this.playAnimation(this.IMAGES_DEAD); // Todesanimation des Hühnchens abspielen
-
-    // Hühnchen nach einer Verzögerung entfernen
-    setTimeout(() => {
-      this.removeObject(); // Huhn aus der Spielwelt entfernen
-    }, 1500);
-  } */
